1b2384dd2eeec108bfe6ae5c4e8a0e91c4bebe27
Mari Sato <marge0101.ms@gmail.com>
2018-02-10 16:20:48 +0000

    advanced login remember me

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersLoginTest < ActionDispatch::IntegrationTest
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
+end
